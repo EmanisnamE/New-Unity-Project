dfef8b08578865e8c064fac708850815aff67f16
EmanisnamE <emanisnamebackwards@gmail.com>
2021-09-28 10:31:38 -0400

    :(

--- Assets/scripts/menu.cs
+++ Assets/scripts/menu.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
-public class player : MonoBehaviour
+public class menu : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    public void Loadlevel(string name)
     {
-        
+        SceneManager.LoadScene(name);
     }
 
     // Update is called once per frame
-    void Update()
+    public void Quit()
     {
-        
+        Application.Quit();
     }
 }
